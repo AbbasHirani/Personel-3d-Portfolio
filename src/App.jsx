41ddf29f0cd1285f55fa61f3efccf1c67a70c58c
AbbasHirani <abbashirani154@gmail.com>
2025-08-17 14:49:05 +0530

    Feature Cards added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero';
 import ShowCase from './sections/ShowCase';
 import Navbar from './components/navbar';
 import LogoSection from './components/LogoSection';
+import FeatureCards from './sections/FeatureCards';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <ShowCase />
       <LogoSection />
+      <FeatureCards />
     </>
     
   );
